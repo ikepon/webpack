333e23019cfd35db502cfeddc6eba4235e7ffb6c
ikepon <tatsuyanoheya@gmail.com>
2017-04-02 11:44:25 +0900

    webpack.config.jsの修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './app/index.js',
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './app.js'
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
   }
 };
