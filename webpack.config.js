2633cae00634baf08e5a74084c8bcc9696fe64e4
ikepon <tatsuyanoheya@gmail.com>
2017-04-02 12:46:11 +0900

    Babel + ES6

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,18 @@ module.exports = {
     path: path.resolve(__dirname, './dist/assets'),
     publicPath: '/assets'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }],
+      }
+    ],
+  },
   devServer: {
     contentBase: path.resolve(__dirname, './src')
   }
